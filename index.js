ebf421bebbba2e9ae51710aef8de170de454be7c
arfanur04 <arfanur04@gmail.com>
2024-04-20 23:21:48 +0600

    55-3 Create Client slide Post

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const userCollection = client.db("userDB").collection("users");
+
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			console.log(`user:`, user);
+			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
